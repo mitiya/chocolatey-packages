2f53807f716126d2f46fd5a5b0e6114a12fa65dd
mitiya <mitdr@yandex.ru>
2018-06-01 12:22:43 +0300

    add

---
+++ chocoguiv2/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'chocoguiv2'
+$url = 'https://github.com/mitiya/chocoGUI/releases/download/0.4/chocoGUIv2.zip'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$shcPath = $ChocolateyInstall + '\bin\chocoGUIv2.exe'
+
+Install-ChocolateyZipPackage -PackageName "$packageName" `
+                             -Url "$url" `
+                             -UnzipLocation "$toolsDir" 
+	
+Install-ChocolateyShortcut -shortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\chocoGUIv2.lnk" -targetPath "$($env:ChocolateyInstall + '\bin\chocoGUIv2.exe')"				 
---
+++ chocoguiv2/tools/chocolateyuninstall.ps1
@@ -0,0 +1,3 @@
+$packageName='chocoguiv2' 
+
+remove-item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\chocoGUIv2.lnk" -Force -ErrorAction 'SilentlyContinue'
