58d8530cd3bde98a2b2322308bcfa44a184b2bc7
mitiya <mitdr@yandex.ru>
2020-04-09 15:22:37 +0300

    spike

---
+++ spike/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'spike'
+$installerType = 'exe'
+$silentArgs = '/S'
+$url = 'https://spikenow.com/web/_dist/win/Spike%20Setup%202.9.1.exe'
+
+
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url
