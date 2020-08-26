0a05c11f2d7ec8b6776e158b8647ee1f251dfc4a
Detlef Schneider <dschneider@across.net>
2020-08-26 09:26:27 +0200

    Ends DesktopInfo64 process if it exists

--- desktopinfo/tools/chocolateybeforemodify.ps1
+++ desktopinfo/tools/chocolateybeforemodify.ps1
@@ -1,3 +1,7 @@
 ﻿if (Get-Process 'DesktopInfo' -ErrorAction SilentlyContinue) {
    Stop-Process -Name DesktopInfo -Force
 }
+
+if (Get-Process 'DesktopInfo64' -ErrorAction SilentlyContinue) {
+   Stop-Process -Name DesktopInfo64 -Force
+}
