a988a7580773c8f54e4f17443f97128447c46ee1
mitiya <mitdr@yandex.ru>
2020-11-08 02:08:33 +0300

    Add files via upload

---
+++ wifiaddcmd/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+
+$url        = 'https://github.com/mitiya/wifiabbcmd/releases/download/' + $env:ChocolateyPackageVersion + '/wifiaddcmd.zip' # download url, HTTPS preferred
+
+$packageZipArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  validExitCodes= @(0, 3010, 1641)
+}
+
+
