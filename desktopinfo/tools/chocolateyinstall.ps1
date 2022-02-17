3aad49b21e22078d95a11d77ca200e2c9eecfe4e
MaximumWoahverdrive <Jo.schmeder.jo@gmail.com>
2022-02-17 12:14:12 +0100

    Update URL, Version and checksum to 3.4.0

--- desktopinfo/tools/chocolateyinstall.ps1
+++ desktopinfo/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://www.glenn.delahoy.com/downloads/desktopinfo/DesktopInfo210.zip' # download url, HTTPS preferred
+$url        = 'https://www.glenn.delahoy.com/downloads/desktopinfo/DesktopInfo3040.zip' # download url, HTTPS preferred
 
 $packageZipArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
   validExitCodes= @(0, 3010, 1641)
-  Checksum = 'fedd560dc83574ed1e2bf7f415a17e73d0fdb5c2287869116b960bd2b48f30ba'
+  Checksum = 'BA668BC5292BC72ED71D1F87D2E39EBD6527E994B41F4381F522F5366B7D796D'
   ChecksumType = 'SHA256'
 }
 
