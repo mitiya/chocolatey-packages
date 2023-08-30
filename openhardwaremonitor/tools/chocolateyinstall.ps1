70c2be7e7a6f75ff029ef6ab6bf061d419ae1f2f
mitiya <mitdr@yandex.ru>
2023-08-30 18:38:57 +0300

    Add files via upload

---
+++ openhardwaremonitor/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+
+
+
+$url        = 'https://openhardwaremonitor.org/files/openhardwaremonitor-v' + $env:ChocolateyPackageVersion + '.zip' # download url, HTTPS preferred
+
+$packageZipArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  validExitCodes= @(0, 3010, 1641)
+}
+
+
+Install-ChocolateyZipPackage @packageZipArgs
